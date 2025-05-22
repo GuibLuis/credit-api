1a5d6da8e1d6c0de8e4759c36263b4cfdb41eebf
Guilherme Quartel <guibluis0811@gmail.com>
2025-05-22 17:12:49 -0300

    documentation update + float to decimal for precision

--- main.py
+++ main.py
@@ -2,32 +2,73 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import decimal
 import requests
+
+description = """
+API para consulta de ofertas de crédito de acordo com o cpf do cliente.
+"""
+
+tags_metadata = [
+    {
+        "name": "consulta",
+        "description": "Consulta de ofertas de crédito de acordo com o cpf do cliente"
+    }
+]
+
+app = FastAPI(title="CreditAPI",
+              description=description,
+              version="0.0.1",
+              contact={
+                  "name": "Guilherme Luis",
+                  "url": "https://github.com/guibluis"
+              },
+              openapi_tags=tags_metadata)
 
 load_dotenv()
 
-app = FastAPI()
-
 urlStartPoint = os.getenv("URL_START_POINT")
+
 
 class Consulta(BaseModel):
     cpf: str
-    valorSolicitado: float = 0
+    valorSolicitado: decimal.Decimal = 0
     parcelas: int = 0
 
-# retorna as ofertas de crédito de acordo com o cpf caso valorSolicitado e parcelas não sejam informados 
+# retorna as ofertas de crédito de acordo com o cpf caso valorSolicitado e parcelas não sejam informados
 # retorna as 3 melhores ofertas de crédito do cpf caso valorSolicitado e parcelas sejam informados
-@app.post("/consulta/")
+
+
+@app.post("/consulta/", tags=["consulta"], responses={
+    200: {
+        "description": "Ofertas de crédito encontradas",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "defaultResult": {
+                        "value": {
+                            "instituicoes": [{"nome": "Banco do Brasil", "modalidades": [
+                                {"nome": "Crédito Consignado", "valorMin": 1000, "valorMax": 10000, "QntParcelaMin": 1, "QntParcelaMax": 12, "jurosMes": 0.01}]}]}
+                    },
+                    "withAllParams": {
+                        "value": {
+                            "bestOffers": [{"instituicaoFinanceira": "Banco do Brasil", "modalidadeCredito": "Crédito Consignado",
+                                            "valorAPagar": 1000, "valorSolicitado": 1000, "valorParcela": 100, "taxaJuros": 0.01, "qntParcelas": 12}]}
+                    }
+                }
+            }
+        }
+    }})
 def consulta_cpf(consulta: Consulta):
     # Consulta quais instituições o cpf tem crédito
     jsonRequest = {"cpf": consulta.cpf}
     response = requests.post(urlStartPoint + "credito/", json=jsonRequest)
-    
+
     # Variaveis para encontrar as 3 melhores ofertas
     melhoresOfertas = []
     valorSolicitado = consulta.valorSolicitado
     parcelas = consulta.parcelas
-    
+
     if response.ok:
         result = response.json()
         resultsPerInstitution = {}
@@ -37,11 +78,13 @@ def consulta_cpf(consulta: Consulta):
             for modalidade in instituicao["modalidades"]:
                 jsonRequest = {
                     "cpf": consulta.cpf, "instituicao_id": instituicao["id"], "codModalidade": modalidade["cod"]
-                    }
-                response = requests.post(urlStartPoint + "oferta/", json=jsonRequest)
-                
+                }
+                response = requests.post(
+                    urlStartPoint + "oferta/", json=jsonRequest)
+
                 if response.ok:
-                    resultsPerInstitution[instituicao["nome"]][modalidade["nome"]] = response.json()
+                    resultsPerInstitution[instituicao["nome"]
+                                          ][modalidade["nome"]] = response.json()
 
         if valorSolicitado != 0 and parcelas != 0:
             # Encontra as 3 melhores ofertas com base no valor a pagar(valorSolicitado + juros)
@@ -49,16 +92,21 @@ def consulta_cpf(consulta: Consulta):
                 for modalidade in resultsPerInstitution[instituicao]:
                     modalidadeValorMinimo = resultsPerInstitution[instituicao][modalidade]["valorMin"]
                     modalidadeValorMaximo = resultsPerInstitution[instituicao][modalidade]["valorMax"]
-                    modalidadeParcelasMinimo = resultsPerInstitution[instituicao][modalidade]["QntParcelaMin"]
-                    modalidadeParcelasMaximo = resultsPerInstitution[instituicao][modalidade]["QntParcelaMax"]
+                    modalidadeParcelasMinimo = resultsPerInstitution[
+                        instituicao][modalidade]["QntParcelaMin"]
+                    modalidadeParcelasMaximo = resultsPerInstitution[
+                        instituicao][modalidade]["QntParcelaMax"]
                     modalidadeJuros = resultsPerInstitution[instituicao][modalidade]["jurosMes"]
-                    
+                    modalidadeJuros = decimal.Decimal(modalidadeJuros)
+
                     if valorSolicitado < modalidadeValorMinimo or valorSolicitado > modalidadeValorMaximo:
                         continue
                     if parcelas < modalidadeParcelasMinimo or parcelas > modalidadeParcelasMaximo:
                         continue
                     if modalidadeValorMinimo <= valorSolicitado <= modalidadeValorMaximo and modalidadeParcelasMinimo <= parcelas <= modalidadeParcelasMaximo:
-                        valorParcela = valorSolicitado * (modalidadeJuros * (1 + modalidadeJuros) ** parcelas) / ((1 + modalidadeJuros) ** parcelas - 1)
+                        valorParcela = (valorSolicitado *
+                                        (modalidadeJuros * (1 + modalidadeJuros) **
+                                         parcelas) / ((1 + modalidadeJuros) ** parcelas - 1))
                         melhoresOfertas.append({
                             "instituicaoFinanceira": instituicao,
                             "modalidadeCredito": modalidade,
@@ -70,7 +118,7 @@ def consulta_cpf(consulta: Consulta):
                         })
             melhoresOfertas.sort(key=lambda x: x["valorAPagar"])
             melhoresOfertas = melhoresOfertas[:3]
-            
+
             return melhoresOfertas
         else:
             return resultsPerInstitution
